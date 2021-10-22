74677b129ad280310af80e4833d1ca3df1fc1ad8
yuikuzuya <yui.kuzuya@asterone.co.jp>
2021-10-22 14:36:32 +0900

    popup の表示

--- js/popup.js
+++ js/popup.js
@@ -1,46 +1,9 @@
-function displayPopup(popupNum) {
-    const num = popupNum;
-    switch (num) {
-        case 0:
-            document.getElementById("logout_popup").style.display = "flex";
-            break;
-        case 1:
-            document.getElementById("logout_success_popup").style.display = "flex";
-            break;
-        case 2:
-            document.getElementById("import_popup").style.display = "flex";
-            break;
-        case 3:
-            document.getElementById("import_success_popup").style.display = "flex";
-            break;
-        case 4:
-            document.getElementById("export_popup").style.display = "flex";
-            break;
-        case 5:
-            document.getElementById("export_success_popup").style.display = "flex";
-            break;
-        case 6:
-            document.getElementById("search_stock_fail_popup").style.display = "flex";
-            break;
-        case 7:
-            document.getElementById("buyer_registration_success_popup").style.display = "flex";
-            break;
-        case 8:
-            document.getElementById("supplier_registration_success_popup").style.display = "flex";
-            break;
-        case 9:
-            document.getElementById("withdraw_success_popup").style.display = "flex";
-            break;
-        case 10:
-            document.getElementById("withdraw_popup").style.display = "flex";
-            break;
-        case 11:
-            document.getElementById("update_stock_success_popup").style.display = "flex";
-            break;
-        case 12:
-            document.getElementById("delete_popup").style.display = "flex";
-            break;
-        default:
-            console.log("失敗しました");
-    }
+
+function displayPopup(popupMsg) {
+    const msg = popupMsg;
+    document.getElementById(msg).style.display = "flex";
+    // backgroundChange;
+}
+function backgroundChange() {
+    documents.getElementById("popup-bg").classList.add("popup-bg-dark");
 }
