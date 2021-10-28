d746c5ff2ddb0ec0b1afab7b0b3d831e9c18775e
yuikuzuya <yui.kuzuya@asterone.co.jp>
2021-10-28 10:01:49 +0900

    テーブルの行の追加（途中）

--- js/addRow.js
+++ js/addRow.js
@@ -1,15 +1,23 @@
 const addButton = document.getElementById("add-btn");
 
-addButton.onclick = function () {
-    console.log("oshitayo");
-}
+addButton.onclick = getTableData;
 function getTableData() {
+    // console.log("oshita");
     // テーブルのIDを取得
     const table = document.getElementById("update-stock-table");
     const newRow = table.insertRow(-1);
 
-    const newCell
+    // テーブルの行数を取得する関数の作成
+    // console.log(table.ariaRowIndex);
+    
     const newCell = newRow.insertCell();
-    const addData = document.createTextNode('山田');
+    const addData = document.createTextNode('6');
     newCell.appendChild(addData);
+
+    newCell = newRow.insertCell();
+    newText = document.createTextNode();
+    newCell.appendChild(newText);
+    newCell = newRow.insertCell();
+    newText = document.createTextNode();
+    newCell.appendChild(newText);
 }
