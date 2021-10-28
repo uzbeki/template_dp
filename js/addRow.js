a2dd78b323956fe7922b70b403115128198d866c
yuikuzuya <yui.kuzuya@asterone.co.jp>
2021-10-28 17:51:40 +0900

    追加ボタンで列の追加（途中）

--- js/addRow.js
+++ js/addRow.js
@@ -1,23 +1,66 @@
 const addButton = document.getElementById("add-btn");
+const table = document.getElementById("update-stock-table");
+addButton.onclick = addRow;
 
-addButton.onclick = getTableData;
-function getTableData() {
-    // console.log("oshita");
-    // テーブルのIDを取得
-    const table = document.getElementById("update-stock-table");
+
+function addRow() {
+
+    // const addRowsNum = countNowTableRows();
+    const addRowsNum = table.rows.length
+
     const newRow = table.insertRow(-1);
+    const numTd = newRow.insertCell();
+    const addNumData = document.createTextNode(addRowsNum);
+    numTd.appendChild(addNumData);
 
-    // テーブルの行数を取得する関数の作成
-    // console.log(table.ariaRowIndex);
-    
-    const newCell = newRow.insertCell();
-    const addData = document.createTextNode('6');
-    newCell.appendChild(addData);
+    const checkTd = newRow.insertCell();
+    const addCheckData = document.createTextNode('');
+    checkTd.appendChild(addCheckData);
 
-    newCell = newRow.insertCell();
-    newText = document.createTextNode();
-    newCell.appendChild(newText);
-    newCell = newRow.insertCell();
-    newText = document.createTextNode();
-    newCell.appendChild(newText);
+    const manufacturerTd = newRow.insertCell();
+    const addManuData = document.createTextNode('');
+    manufacturerTd.appendChild(addManuData);
+
+    const productTd = newRow.insertCell();
+    const addProductData = document.createTextNode('');
+    productTd.appendChild(addProductData);
+
+    const modelTd = newRow.insertCell();
+    const addModelData = document.createTextNode('');
+    modelTd.appendChild(addModelData);
+
+    const quantityTd = newRow.insertCell();
+    const addQuantityData = document.createTextNode('');
+    quantityTd.appendChild(addQuantityData);
+
+    const packingTd = newRow.insertCell();
+    const addPackingData = document.createTextNode('');
+    packingTd.appendChild(addPackingData);
+
+    const rohsTd = newRow.insertCell();
+    const addRohsData = document.createTextNode('');
+    rohsTd.appendChild(addRohsData);
+
+    const dcTd = newRow.insertCell();
+    const addDcData = document.createTextNode('');
+    dcTd.appendChild(addDcData);
+
+    const remarksTd = newRow.insertCell();
+    const addRemarksData = document.createTextNode('');
+    remarksTd.appendChild(addRemarksData);
+}
+
+// テーブルの行数を数える
+function countNowTableRows() {
+    const rowsLength = table.rows.length
+    return rowsLength;
+}
+
+// tdに要素の追加
+function addElement() {
+    const newInput = document.createElement("input");
+    newInput.type = "checkbox";
+    // ID取得以外で、要素を挿入したい
+    const parent = document.getElementById('');
+    appendChild(newInput);
 }
